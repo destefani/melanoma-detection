9579c740b375647ca7d3355ce55cc0a00ed7fa9a
Marcelo Destefani <marcelo@hastapronto.cl>
2022-01-06 18:30:16 +0100

    resnet network file - tested

---
+++ training/networks.py
@@ -0,0 +1,38 @@
+import torch
+import torchvision
+
+
+class ResNet(torch.nn.Module):
+    def __init__(self,
+        resnet_type = "resnet18",
+        pretrained = True,
+        num_classes = 2
+        ):
+        super(ResNet, self).__init__()
+
+        # Initialize resnet model
+        if resnet_type == "resnet18":
+            self.resnet = torchvision.models.resnet18(pretrained=True)
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+            self.resnet.fc = torch.nn.Linear(512, num_classes)
+            for param in self.resnet.fc.parameters():
+                param.requires_grad = False
+        if resnet_type == "resnet50":
+            self.resnet = torchvision.models.resnet50(pretrained=True)
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+            self.resnet.fc = torch.nn.Linear(2048, num_classes)
+            for param in self.resnet.fc.parameters():
+                param.requires_grad = False
+        if resnet_type == "resnet101":
+            self.resnet = torchvision.models.resnet101(pretrained=True)
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+            self.resnet.fc = torch.nn.Linear(2048, num_classes)
+            for param in self.resnet.fc.parameters():
+                param.requires_grad = False
+    
+    def forward(self, x):
+        x = self.resnet(x)
+        return x
