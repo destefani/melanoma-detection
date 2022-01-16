e6b2ad23eb3a780719c22563fb37a5d1d58eb4d1
Marcelo Destefani <marcelo@hastapronto.cl>
2022-01-16 20:48:08 +0100

    update

--- training/dataset.py
+++ training/dataset.py
@@ -1,8 +1,14 @@
+import os
+import sys
 import torch
 import numpy as np
 import PIL.Image
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
+from monai.data import CacheDataset, PersistentDataset
+from monai.transforms import Randomizable
+
 
 
 class ISIC2020(torch.utils.data.Dataset):
@@ -25,7 +31,7 @@ class ISIC2020(torch.utils.data.Dataset):
         self.test = test
         self.test_size = test_size
         self.seed = seed
-
+        
         # Split the dataset into train and test
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             self.annotations["image_name"],
@@ -55,3 +61,141 @@ class ISIC2020(torch.utils.data.Dataset):
             image = self.transform(image)
 
         return image, label
+
+
+class CacheISIC2020(CacheDataset):
+    "ISIC-2020 Dataset"
+
+    def __init__(self,
+        csv_file,                   # .csv file with image names and labels
+        root_dir,                   # Root directory of the dataset
+        transform,                  # Transform to be applied to the images
+        cache_dir   = "cache_dir/", # Directory to cache the dataset
+        format      = ".jpg",       # Format of the images
+        test        = False,        # Whether to use the test set
+        test_size   = 0.2,          # Size of the test set
+        cache_rate  = 1.0,
+        cache_num   = sys.maxsize,  # Number of images to cache
+        seed        = 42,           # Random seed
+        num_workers = 8,
+        warmup      = False,
+        ):
+
+        self.annotations = pd.read_csv(csv_file)
+        if warmup:
+            self.annotations = self.annotations.iloc[:500]
+        self.annotations["image"] = self.annotations["image_name"].apply(lambda x: os.path.join(root_dir, x + format))
+        self.root_dir = root_dir
+        self.format = format
+        self.transform = transform
+        self.test = test
+        self.test_size = test_size
+        self.seed = seed
+        
+        # Split the dataset into train and test
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.annotations["image"],
+            self.annotations["target"],
+            test_size=self.test_size,
+            random_state=self.seed,
+            stratify=self.annotations["target"],
+        )
+
+        if test:
+            data = pd.concat([self.X_test, self.y_test], axis=1).to_dict(orient="records")
+        else:
+            data = pd.concat([self.X_train, self.y_train], axis=1).to_dict(orient="records")
+        super().__init__(
+            data, transform=transform, 
+            cache_rate=cache_rate, cache_num=cache_num,
+            num_workers=num_workers
+            )
+
+
+# class PersistISIC2020(Randomizable, PersistentDataset):
+#     "ISIC-2020 Dataset"
+
+#     def __init__(self,
+#         csv_file,                  # .csv file with image names and labels
+#         root_dir,                  # Root directory of the dataset
+#         transform,                 # Transform to be applied to the images
+#         cache_dir  = "cache_dir/", # Directory to cache the dataset
+#         format     = ".jpg",       # Format of the images
+#         test       = False,        # Whether to use the test set
+#         test_size  = 0.2,          # Size of the test set
+#         cache_rate = 1.0,
+#         seed       = 42,           # Random seed
+#         ):
+
+#         self.annotations = pd.read_csv(csv_file)
+#         self.annotations["image"] = self.annotations["image_name"].apply(lambda x: os.path.join(root_dir, x + format))
+#         self.root_dir = root_dir
+#         self.format = format
+#         self.transform = transform
+#         self.test = test
+#         self.test_size = test_size
+#         self.seed = seed
+        
+#         # Split the dataset into train and test
+#         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+#             self.annotations["image"],
+#             self.annotations["target"],
+#             test_size=self.test_size,
+#             random_state=self.seed,
+#             stratify=self.annotations["target"],
+#         )
+
+#         if test:
+            
+#             data = pd.concat([self.X_train, self.y_train], axis=1).to_dict(orient="records")
+#         else:
+#             data = pd.concat([self.X_train, self.y_train], axis=1).to_dict(orient="records")
+#         super().__init__(
+#             data, transform=transform, 
+#             cache_dir=cache_dir
+#             )
+
+
+class PersistISIC2020(PersistentDataset):
+    "ISIC-2020 Dataset"
+
+    def __init__(self,
+        csv_file,                   # .csv file with image names and labels
+        root_dir,                   # Root directory of the dataset
+        transform,                  # Transform to be applied to the images
+        cache_dir   = "cache_dir/", # Directory to cache the dataset
+        format      = ".jpg",       # Format of the images
+        test        = False,        # Whether to use the test set
+        test_size   = 0.2,          # Size of the test set
+        cache_rate  = 1.0,
+        cache_num   = sys.maxsize,  # Number of images to cache
+        seed        = 42,           # Random seed
+        num_workers = 8,
+        warmup      = False,
+        ):
+
+        self.annotations = pd.read_csv(csv_file)
+        if warmup:
+            self.annotations = self.annotations.iloc[:500]
+        self.annotations["image"] = self.annotations["image_name"].apply(lambda x: os.path.join(root_dir, x + format))
+        self.root_dir = root_dir
+        self.format = format
+        self.transform = transform
+        self.test = test
+        self.test_size = test_size
+        self.seed = seed
+        
+        # Split the dataset into train and test
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.annotations["image"],
+            self.annotations["target"],
+            test_size=self.test_size,
+            random_state=self.seed,
+            stratify=self.annotations["target"],
+        )
+
+        if test:
+            data = pd.concat([self.X_test, self.y_test], axis=1).to_dict(orient="records")
+        else:
+            data = pd.concat([self.X_train, self.y_train], axis=1).to_dict(orient="records")
+        super().__init__(data, transform=transform, cache_dir=cache_dir)
--- training/networks.py
+++ training/networks.py
@@ -6,13 +6,13 @@ class ResNet(torch.nn.Module):
     def __init__(self,
         resnet_type = "resnet18",
         pretrained = True,
-        num_classes = 2,
+        num_classes = 1,
         ):
         super(ResNet, self).__init__()
 
         # Initialize resnet model
         if resnet_type == "resnet18":
-            self.resnet = torchvision.models.resnet18(pretrained=True)
+            self.resnet = torchvision.models.resnet18(pretrained=pretrained)
             for param in self.resnet.parameters():
                 param.requires_grad = False
             self.resnet.fc = torch.nn.Linear(512, num_classes)
@@ -20,7 +20,7 @@ class ResNet(torch.nn.Module):
                 param.requires_grad = True
 
         if resnet_type == "resnet50":
-            self.resnet = torchvision.models.resnet50(pretrained=True)
+            self.resnet = torchvision.models.resnet50(pretrained=pretrained)
             for param in self.resnet.parameters():
                 param.requires_grad = False
             self.resnet.fc = torch.nn.Linear(2048, num_classes)
@@ -28,7 +28,15 @@ class ResNet(torch.nn.Module):
                 param.requires_grad = True
                 
         if resnet_type == "resnet101":
-            self.resnet = torchvision.models.resnet101(pretrained=True)
+            self.resnet = torchvision.models.resnet101(pretrained=pretrained)
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+            self.resnet.fc = torch.nn.Linear(2048, num_classes)
+            for param in self.resnet.fc.parameters():
+                param.requires_grad = True
+
+        if resnet_type == "resnet152":
+            self.resnet = torchvision.models.resnet152(pretrained=pretrained)
             for param in self.resnet.parameters():
                 param.requires_grad = False
             self.resnet.fc = torch.nn.Linear(2048, num_classes)
@@ -37,4 +45,23 @@ class ResNet(torch.nn.Module):
     
     def forward(self, x):
         x = self.resnet(x)
+        return x
+
+class EfficientNet7(torch.nn.Module):
+    def __init__(self,
+        pretrained = True,
+        num_classes = 1,
+        ):
+        super(EfficientNet7, self).__init__()
+
+        # Initialize resnet model
+        self.model = torchvision.models.efficientnet_b7(pretrained=pretrained)
+        for param in self.model.parameters():
+            param.requires_grad = False
+        self.model.classifier[1] = torch.nn.Linear(2560, num_classes)
+        for param in self.model.classifier[1].parameters():
+            param.requires_grad = True
+    
+    def forward(self, x):
+        x = self.model(x)
         return x
