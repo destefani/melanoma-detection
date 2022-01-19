b17f918ca658d180d6db2423cf7a3e78fbf0d4a8
Marcelo Destefani <marcelo@hastapronto.cl>
2022-01-19 11:05:59 +0100

    updated datasets

--- train_classifier.py
+++ train_classifier.py
@@ -1,21 +1,16 @@
 import os
 import torch
 from torch import nn
-from torch.utils.data import DataLoader, WeightedRandomSampler
+from torch.utils.data import DataLoader
 import pandas as pd
 import numpy as np
 import wandb
 
 import monai
 from monai.config import print_config
-from monai.data import decollate_batch
+from monai.data import CacheDataset
 from monai.engines import SupervisedEvaluator, SupervisedTrainer
-from monai.handlers import (
-    CheckpointSaver,
-    StatsHandler,
-    ValidationHandler,
-    ROCAUC
-)
+from monai.handlers import CheckpointSaver, StatsHandler, ValidationHandler, ROCAUC
 from monai.networks.nets import DenseNet121, DenseNet201
 
 from monai.transforms import (
@@ -33,13 +28,14 @@ from monai.transforms import (
     EnsureType,
     NormalizeIntensityd,
     Resized,
-    ToTensord
+    ToTensord,
 )
 from monai.utils import set_determinism
 from ignite.contrib.handlers.wandb_logger import *
 from ignite.engine import Events
 
-from training.dataset import CacheISIC2020
+from training.data import prepare_data, weighted_sampler
+
 
 print_config()
 
@@ -47,11 +43,12 @@ print_config()
 wandb.init(project="melanoma-detection", entity="mdestefani")
 results_dir = os.path.join("results", wandb.run.name)
 
-root_dir = "dataset/isic/train/"
-annotations_csv = "dataset/isic/ISIC_2020_Training_GroundTruth_v2.csv"
+ROOT_DIR = "dataset/siim-isic-melanoma-classification/jpeg/train"
+LABELS_DF = "dataset/siim-isic-melanoma-classification/train.csv"
 
 if not os.path.isdir(results_dir):
     os.makedirs(results_dir)
+
 
 def main():
     wandb.config.update(
@@ -59,19 +56,19 @@ def main():
             "epochs": 50,
             "batch_size": 128,
             "n_workers": 8,
-            "learning_rate": 1e-3,
-            "lambda_l2": 1e-5,
+            "learning_rate": 0.001,
+            "lambda_l2": 0,
             "model": "densenet121",
             "pretrained": True,
-            "warmup": False,
-            "cache_rate": 1.0
+            "warmup": True,
+            "cache_rate": 0.05,
         }
     )
 
     print(wandb.config)
 
     train_transforms = Compose(
-        [   
+        [
             LoadImaged("image", image_only=True),
             AsChannelFirstd("image"),
             Resized("image", (256, 256)),
@@ -82,53 +79,42 @@ def main():
             Rand2DElasticd("image", (0.3, 0.3), (1.0, 2.0)),
             RandZoomd("image", min_zoom=0.9, max_zoom=1.1, prob=0.5),
             ToTensord("image"),
-        ])
+        ]
+    )
 
     val_transforms = Compose(
-        [   
+        [
             LoadImaged("image", image_only=True),
             AsChannelFirstd("image"),
             Resized("image", (256, 256)),
             CenterSpatialCropd("image", 224),
             NormalizeIntensityd("image"),
             ToTensord("image"),
-        ])
+        ]
+    )
 
     # Create datasets
-    train_dataset = CacheISIC2020(
-        annotations_csv,
-        root_dir=root_dir,
+    train_data, val_data = prepare_data(ROOT_DIR, LABELS_DF, warmup=wandb.config.warmup)
+
+    train_ds = CacheDataset(
+        data=train_data,
         transform=train_transforms,
-        test=False,
-        test_size=0.1,
-        seed=42,
+        num_workers=8,
         cache_rate=wandb.config.cache_rate,
-        warmup=wandb.config.warmup
     )
 
-    val_dataset = CacheISIC2020(
-        annotations_csv,
-        root_dir=root_dir,
+    val_ds = CacheDataset(
+        data=val_data,
         transform=val_transforms,
-        test=True,
-        test_size=0.2,
-        seed=42,
+        num_workers=8,
         cache_rate=wandb.config.cache_rate,
-        warmup=wandb.config.warmup
     )
 
-
-    def get_sampler(labels_df):
-        unique_labels, count = np.unique(labels_df, return_counts=True)
-        label_weight = [sum(count) / c for c in count]
-        weights = [label_weight[e] for e in labels_df]
-        return WeightedRandomSampler(weights, len(weights))
-
-
-    sampler = get_sampler(train_dataset.y_train)
+    # Manage class imbalance
+    sampler = weighted_sampler(train_data)
 
     train_loader = DataLoader(
-        train_dataset,
+        train_ds,
         wandb.config.batch_size,
         num_workers=wandb.config.n_workers,
         sampler=sampler,
@@ -136,7 +122,7 @@ def main():
     )
 
     val_loader = DataLoader(
-        val_dataset,
+        val_ds,
         wandb.config.batch_size,
         num_workers=wandb.config.n_workers,
         shuffle=True,
@@ -145,19 +131,17 @@ def main():
 
     # Define network and optimizer
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print('Device:', device)
-
+    print("Device:", device)
 
     model = DenseNet121(
         spatial_dims=2,
         in_channels=3,
         out_channels=1,
-        pretrained=wandb.config.pretrained
-        ).to(device)
+        pretrained=wandb.config.pretrained,
+    ).to(device)
     wandb.watch(model)
 
-
-    criterion = nn.BCEWithLogitsLoss() # sigmoid + binary cross entropy loss
+    criterion = nn.BCEWithLogitsLoss()  # sigmoid + binary cross entropy loss
     print(criterion)
 
     optimizer = torch.optim.Adam(
@@ -167,18 +151,17 @@ def main():
     )
     print(optimizer)
 
-
     iter_losses = []
     batch_sizes = []
     epoch_loss_values = []
     metric_values = []
 
-    steps_per_epoch = len(train_dataset) // train_loader.batch_size
-    if len(train_dataset) % train_loader.batch_size != 0:
+    steps_per_epoch = len(train_ds) // train_loader.batch_size
+    if len(train_ds) % train_loader.batch_size != 0:
         steps_per_epoch += 1
 
     def prepare_batch(batchdata, device, non_blocking):
-        img, classes = batchdata["image"], batchdata["target"].unsqueeze(dim=1).float()
+        img, classes = batchdata["image"], batchdata["label"].unsqueeze(dim=1).float()
         return img.to(device), classes.to(device)
 
     def roc_auc_trans(x):
@@ -189,7 +172,9 @@ def main():
         return x["pred"], x["label"]
 
     val_handlers = [
-        CheckpointSaver(save_dir=results_dir, save_dict={"model": model}, save_key_metric=True),
+        CheckpointSaver(
+            save_dir=results_dir, save_dict={"model": model}, save_key_metric=True
+        ),
     ]
 
     evaluator = SupervisedEvaluator(
@@ -201,7 +186,7 @@ def main():
         prepare_batch=prepare_batch,
         decollate=False,
         val_handlers=val_handlers,
-        amp=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False
+        amp=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False,
     )
 
     trainer = SupervisedTrainer(
@@ -213,7 +198,7 @@ def main():
         loss_function=criterion,
         train_handlers=[ValidationHandler(1, evaluator)],
         prepare_batch=prepare_batch,
-        amp=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False
+        amp=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False,
     )
 
     @trainer.on(Events.ITERATION_COMPLETED)
@@ -226,8 +211,9 @@ def main():
         iter_losses.append(loss)
         batch_sizes.append(batch_len)
 
-        print(f"Train - epoch {epoch}/{epoch_len}, step {step}/{steps_per_epoch}, training_loss = {loss:.4f}")
-
+        print(
+            f"Train - epoch {epoch}/{epoch_len}, step {step}/{steps_per_epoch}, training_loss = {loss:.4f}"
+        )
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def run_validation(engine):
@@ -243,11 +229,13 @@ def main():
         roc = evaluator.state.metrics["roc_auc"]
         metric_values.append(roc)
         print(f"Eval- epoch {engine.state.epoch}, rocauc = {roc:.4f}")
-        wandb.log({
-            "epoch": engine.state.epoch,
-            "train_loss": overall_average_loss,
-            "auc_score": roc,
-        })
+        wandb.log(
+            {
+                "epoch": engine.state.epoch,
+                "train_loss": overall_average_loss,
+                "auc_score": roc,
+            }
+        )
 
     trainer.run()
 
